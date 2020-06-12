acbf50643c13ce9457add6c545211cdc30c78ef2
Evan hameed <evanhameed9966@gmail.com>
2020-06-12 05:52:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,3 +14,19 @@ function createEmployeeRecords(array){
 let records = array.map(obj=> createEmployeeRecord(obj))
 return records
 }
+
+function createEmployeeRecord(array) {
+  return {
+    firstName: array[0],
+    familyName: array[1],
+    title: array[2],
+    payPerHour: array[3],
+    timeInEvents: [],
+    timeOutEvents: [],
+  };
+}
+
+function createEmployeeRecords(array){
+let records = array.map(obj=> createEmployeeRecord(obj))
+return records
+}
